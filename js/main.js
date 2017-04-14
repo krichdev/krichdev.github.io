2392df3ebd5f1b207ec933cff2ca8f74e18619e1
krichdev <kgrichardson2@gmail.com>
2017-04-14 14:39:34 -0700

    got rid of mobile nav bar and adjusted back to top arrow

--- js/main.js
+++ js/main.js
@@ -1,11 +1,6 @@
 $( document ).ready(function(){
     $(".button-collapse").sideNav();
     $(window).scroll(function() {
-        console.log($(window).scrollTop())
-        // if($(window).scrollTop() + $(window).height() == $(document).height()) {
-        //     $("#fabBtn").show();
-        // }
-
         if($(window).scrollTop() >= 500){
             $("#fabBtn").show()
         }else {
