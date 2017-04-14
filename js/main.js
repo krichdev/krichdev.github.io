56a475c4ebe2eb803b445d93f82d982cee1ba971
krichdev <kgrichardson2@gmail.com>
2017-04-14 14:21:23 -0700

    added fixed button to send to top

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 $( document ).ready(function(){
     $(".button-collapse").sideNav();
+    $(window).scroll(function() {
+        console.log($(window).scrollTop())
+        // if($(window).scrollTop() + $(window).height() == $(document).height()) {
+        //     $("#fabBtn").show();
+        // }
+
+        if($(window).scrollTop() >= 500){
+            $("#fabBtn").show()
+        }else {
+            $("#fabBtn").hide()
+        }
+    });
 });
