ecd58fe08894d2f774be3f864423fb57a22e6aac
krichdev <kgrichardson2@gmail.com>
2018-06-19 18:50:55 -0700

    wired up netlify form function into the contact form #3

--- src/components/contact.js
+++ src/components/contact.js
@@ -3,53 +3,53 @@ import Link from 'gatsby-link'
 import styled from 'styled-components'
 
 const ContactWrapper = styled.div`
-    width: 100%;
-    padding: 1.8em 0;
-    background-color: #164675;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    color: ghostwhite;
+  width: 100%;
+  padding: 1.8em 0;
+  background-color: #164675;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: ghostwhite;
 `
 const ContactForm = styled.form`
-    input, textarea {
-      width: 80vw;
-      display: block;
-      padding: 12px 20px;
-      margin: 10px 0;
-      border: 1px solid #ccc;
-      border-radius: 3px;
+  input,
+  textarea {
+    width: 80vw;
+    display: block;
+    padding: 12px 20px;
+    margin: 10px 0;
+    border: 1px solid #ccc;
+    border-radius: 3px;
+  }
+
+  input[type='submit'] {
+    width: 80vw;
+    background-color: #c1111d;
+    color: ghostwhite;
+    cursor: pointer;
+    padding: 14px 20px;
+    margin: 20px 0;
+    border-radius: 3px;
+    border: none;
+    &:hover {
+      background-color: #ad0f1a;
     }
-    
-    input[type=submit] {
-      width: 80vw;
-      background-color: #c1111d;
-      color: ghostwhite;
-      cursor: pointer;
-      padding: 14px 20px;
-      margin: 20px 0;
-      border-radius: 3px;
-      border: none;
-      &:hover {
-        background-color: #ad0f1a;
-      }
-    }
+  }
 `
 
 const Contact = () => (
   <ContactWrapper>
     <h2>Get In Touch</h2>
-    <ContactForm>
+    <ContactForm name="contact" method="POST" action="/#" data-netlify="true">
       <input type="text" placeholder="Your Name" required />
 
       <input type="email" placeholder="Your E-mail" required />
 
       <textarea placeholder="Type your message" required />
-
+      <div data-netlify-recaptcha />
       <input type="submit" value="Contact Me" />
-
     </ContactForm>
-  </ContactWrapper >
+  </ContactWrapper>
 )
 
 export default Contact
