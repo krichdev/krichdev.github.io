247e94f0c8bcf305ee887c6c9a6297c022a47b0f
krichdev <kgrichardson2@gmail.com>
2018-06-22 01:35:58 -0700

    started blog section layout and design

--- src/components/blog.js
+++ src/components/blog.js
@@ -3,26 +3,41 @@ import Link from 'gatsby-link'
 import styled from 'styled-components'
 
 const BlogWrapper = styled.div`
-    width: 100%;
-    height: 400px;
-    padding: 1.8em 0;
-    display: flex;
-    justify-content: center;
-    h2, h3 {
-        color: #164675;
-    }
+  width: 100%;
+  padding: 1.8em 0;
+  display: flex;
+  justify-content: center;
+  h2,
+  h3 {
+    color: #164675;
+  }
 `
 const BlogContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    width: 90%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 90%;
+`
+const BlogProjectCard = styled.div`
+  width: 80vw;
+  height: 200px;
+  margin-bottom: 20px;
+  border-radius: 3px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  transition: all 0.2s ease-in-out;
+  &:hover {
+    transform: scale(1.03);
+  }
 `
 
 const Blog = () => (
   <BlogWrapper>
-    <h2>From my blog</h2> 
+    <BlogContainer>
+      <h2>Blog</h2>
+      <BlogProjectCard />
+      <BlogProjectCard />
+      <BlogProjectCard />
+    </BlogContainer>
   </BlogWrapper>
 )
 
