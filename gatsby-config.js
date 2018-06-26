79615022e9cb3b2a1bb4213875950b36263d0be0
Kyle Richardson - TireBuyer <kyrichardson@tirebuyer.com>
2018-06-26 13:50:04 -0700

    #9 - changed package.json file and offline plugin to work correctly

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,36 +29,6 @@ module.exports = {
     'gatsby-transformer-remark',
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
-    {
-      resolve: 'gatsby-plugin-favicon',
-      options: {
-        logo: "./src/images/favicon.png",
-        injectHTML: true,
-        icons: {
-          android: true,
-          appleIcon: true,
-          appleStartup: true,
-          favicons: true,
-          coast: false,
-          firefox: true,
-          twitter: false,
-          yandex: false,
-          windows: false
-        }
-      }
-    },
-    {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        name: `Hello I'm Kyle`,
-        short_name: `Hello I'm Kyle`,
-        start_url: '/',
-        background_color: '#164675',
-        theme_color: "#a2466c",
-        display: "minimal-ui",
-        icon: "./src/images/favicon.png"
-      }
-    },
     'gatsby-plugin-offline',
   ],
 }
