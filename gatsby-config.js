ccd5417cac84ce0d4dca1fe612fdd7c5191f3060
Kyle Richardson - TireBuyer <kyrichardson@tirebuyer.com>
2018-06-26 14:03:03 -0700

    #9 - added manifest for android devices

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,18 @@ module.exports = {
     'gatsby-transformer-remark',
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Hello I'm Kyle`,
+        short_name: `Hello I'm Kyle`,
+        start_url: `/`,
+        background_color: `#164675`,
+        theme_color: `#164675`,
+        display: `minimal-ui`,
+        icon: `./src/images/favicon.png`
+      },
+    },
     'gatsby-plugin-offline',
   ],
 }
